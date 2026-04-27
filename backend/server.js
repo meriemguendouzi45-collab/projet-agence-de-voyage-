e100cacd253008b1f5af811d0139240bb99c13ce
guendouzi meriem <meriemguendouzi45@gmail.com>
2026-04-27 15:13:14 +0100

    fix: update server port for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use("/api/services", require("./routes/serviceRoutes"));
 app.use("/api/subscribers", require("./routes/subscriberRoutes"));
 
 // ✅ PORT (important for Render)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
